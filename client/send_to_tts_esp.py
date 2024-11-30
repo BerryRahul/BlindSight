3fed797e0c2498961c0dedefce44983a49d84739
Rahul Berry <113147217+BerryRahul@users.noreply.github.com>
2024-11-30 12:29:31 -0500

    Update send_to_tts_esp.py

--- client/send_to_tts_esp.py
+++ client/send_to_tts_esp.py
@@ -12,14 +12,11 @@ if not os.path.exists(output_folder):
 def transfer_file_when_ready(server, user, remote_file_path, local_file_path, check_interval=0.5):
     while True:
         try:
-            # Check if the file exists on the server
             result = subprocess.run(
                 ["ssh", f"{user}@{server}", f"test -f {remote_file_path}"],
                 capture_output=True
             )
-            if result.returncode == 0:  # File exists
-                # Use scp to transfer the file
-                # os.system('rm -r sharpest_frame.txt')
+            if result.returncode == 0:  
                 subprocess.run(
                     ["scp", f"{user}@{server}:{remote_file_path}", local_file_path],
                     check=True
@@ -32,7 +29,6 @@ def transfer_file_when_ready(server, user, remote_file_path, local_file_path, ch
             print(f"Error: {e}")
         time.sleep(check_interval)
 
-# Example Usage
 server = "195.242.13.247"
 user = "ubuntu"
 remote_file_path = "/home/ubuntu/meta_llama_hacks/responds/sharpest_frame.txt"
@@ -50,20 +46,15 @@ print(message)
 ESP32_IP = "192.168.155.27"  # Replace with the ESP32's IP address
 ESP32_PORT = 80             # Port the ESP32 server is listening on
 
-# Create a socket object
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
-    # Connect to the ESP32 server
     client_socket.connect((ESP32_IP, ESP32_PORT))
     print("Connected to ESP32")
 
-    # Send a string
-    # message = "Hello from Python!\n"  # '\n' marks the end of the message
     client_socket.send(message.encode())
     print("Message sent:", message)
 
-    # Optional: Receive acknowledgment
     response = client_socket.recv(1024).decode()
     print("Response from ESP32:", response)
 
